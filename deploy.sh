b164c3575661674996986872d7df4dbd001884dc
Karl Hughes <khughes.me@gmail.com>
2019-01-18 21:03:13 -0600

    Supressing output of curl

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 echo Pushing update to "$RANCHER_BASE_URL"/v3/project/c-s48ls:p-26zxx/workloads/deployment:default:feed-functions
 
-curl -X PUT \
+curl -s -o /dev/null -w "%{http_code}" -X PUT \
   "$RANCHER_BASE_URL"/v3/project/c-s48ls:p-26zxx/workloads/deployment:default:feed-functions \
   -H "Authorization: Bearer $RANCHER_API_BEARER_TOKEN" \
   -H 'Content-Type: application/json' \
